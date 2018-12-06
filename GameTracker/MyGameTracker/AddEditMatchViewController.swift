b4ee1e2baa39c4666eaed2e26009b96da2d82133
Clemens Stift <clemens@stift.online>
2018-12-06 11:47:35 +0100

    Added MatchesView LocalizableStrings.

---
+++ GameTracker/MyGameTracker/AddEditMatchViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddEditMatchViewController.swift
+//  MyGameTracker
+//
+//  Created by Clemens Stift on 06.12.18.
+//  Copyright © 2018 Clemens Stift. All rights reserved.
+//
+
+import Foundation
